c1c17d335b26d1f246c8b5fa185c95242f86cb37
ivanov.st <ivanov.st@isoft.bg>
2020-10-22 14:22:33 +0300

    Updates

---
+++ precache-manifest.bc22bcc78793c99f196b9964497ee8ea.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7f9c51a2e2de8f858e51c01dcc2afc9f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dad9b80d68aeec3eb528",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "f50d2f31b2b7d4f444dd",
+    "url": "/static/js/2.1c922d4c.chunk.js"
+  },
+  {
+    "revision": "65075b20798fa24012a1ad453c78c90d",
+    "url": "/static/js/2.1c922d4c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dad9b80d68aeec3eb528",
+    "url": "/static/js/main.78e7443d.chunk.js"
+  },
+  {
+    "revision": "289c3f0b6392671afc97",
+    "url": "/static/js/runtime-main.6e989a82.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
